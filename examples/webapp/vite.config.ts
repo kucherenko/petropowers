1814b0ffafed3dece1f3ebee46528a68ee3007c2
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2026-04-06 18:06:10 +0200

    feat(webapp): scaffold Svelte 5 + Vite + Tailwind project

---
+++ examples/webapp/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+export default defineConfig({
+  plugins: [svelte({ hot: !process.env.VITEST })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    include: ['src/**/*.test.ts'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
